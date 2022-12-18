742a952165313e142c8bc74fe8d2165a1b5bb4d9
yuvrajsinh zala <75449804+Zala1999@users.noreply.github.com>
2022-12-18 17:51:23 +0300

    Add files via upload

---
+++ HostalMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.hostal.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostalMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ HostelMangementApplication.java
@@ -0,0 +1,12 @@
+package in.co.hostel.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelMangementApplication.class, args);
+	}
+}
